79ea9abd0435dd473b5628a5029f7bcf7afd1627
Jackly654 <jackly65431@gmail.com>
2015-12-16 17:43:58 +0800

    The Three Body Start

---
+++ chapter8/05/scripts/addLoadEvent.js
@@ -0,0 +1,12 @@
+//方法队列
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if(typeof window.onload != "function"){
+		window.onload = func;
+	}else{
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}

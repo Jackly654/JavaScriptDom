fd06d0e87e8e1992dfc38c7e596f112eefdd2b4f
Jackly654 <jackly65431@gmail.com>
2015-12-22 17:25:10 +0800

    animation&h5

--- chapter10/0141/scripts/positionMessage.js
+++ chapter10/0141/scripts/positionMessage.js
@@ -6,6 +6,12 @@ function positionMessage(x,y){
   elem.style.left = "50px";
   elem.style.top = "100px";
   moveElement("message",200,500,10);
+  if(!document.getElementById("message2")) return false;
+  var elem = document.getElementById("message2");
+  elem.style.position = "absolute";
+  elem.style.left = "50px";
+  elem.style.top = "50px";
+  moveElement("message2",125,25,20);
 }
 
 addLoadEvent(positionMessage);
